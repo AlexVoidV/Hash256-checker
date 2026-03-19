d8ae5833d6fc143eaf2595a63a9d40f571a56e72
Alex <81166439+AlexCodeMan@users.noreply.github.com>
2026-03-19 17:16:46 +0700

    feat: Option for heavy files

--- hash_checker.py
+++ hash_checker.py
@@ -1,10 +1,17 @@
 import hashlib as hsh
 
 
-def calculate_file_hash(file_path):
-    with open(file_path, "rb") as f:
-        digest = hsh.file_digest(f, "sha256")
-    return digest.hexdigest()
+def calculate_file_hash(file_path, variant):
+    if variant == "y":
+        hash_obj = hsh.sha256()
+        with open(file_path, "rb") as f:
+            while chunk := f.read(4096):
+                hash_obj.update(chunk)
+        return hash_obj.hexdigest()
+    else:
+        with open(file_path, "rb") as f:
+            digest = hsh.file_digest(f, "sha256")
+        return digest.hexdigest()
 
 
 def check_hashes(hash1, hash2):
@@ -15,12 +22,13 @@ def check_hashes(hash1, hash2):
 
 
 def main():
+    variant = input("Ваш файл тяжелее 1 ГБ? (y / n): ").strip().lower()
     while True:
         try:
             # Путь к файлу
             file_path = input("Укажите путь к файлу: ").strip()
             # Преобразование в строку хэш файла
-            file_hash = calculate_file_hash(file_path)
+            file_hash = calculate_file_hash(file_path, variant)
             break
         except FileNotFoundError:
             print("Файл не найден!")
@@ -30,7 +38,7 @@ def main():
     print(f"Хэш указанного файла: {file_hash}")
     print(f"Введённый хэш: {user_hash}")
     # Результат
-    if check_hashes(file_hash, user_hash): #  == True
+    if check_hashes(file_hash, user_hash):  # == True
         print("Хэш совпадает")
     else:
         print("Хэш не совпал!")
